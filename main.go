577840e899bc58285f9bfc3080601a065c203300
ander2812 <ander2812@gmail.com>
2021-09-12 17:05:12 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	//"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var temp = template.Must(template.ParseGlob("template/*"))
+
+func main() {
+	http.HandleFunc("/", Init)
+	http.HandleFunc("/create", Create)
+
+	log.Println("Servidor corriendo...")
+	http.ListenAndServe(": 8080", nil)
+}
+
+func Init(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprint(w, "Hola develoteca")
+	temp.ExecuteTemplate(w, "init", nil)
+
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprint(w, "Hola develoteca")
+	temp.ExecuteTemplate(w, "create", nil)
+
+}
