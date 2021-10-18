b55ccad2cf6d63bb8ff2747d45b660a9bf2f21ba
ander2812 <ander2812@gmail.com>
2021-10-18 15:21:21 -0500

    data base modified

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func conexionBD() (conexion *sql.DB) {
 	Driver := "mysql"
 	Usuario := "root"
 	Contrasenia := ""
-	Nombre := "sistema"
+	Nombre := "system"
 
 	conexion, err := sql.Open(Driver, Usuario+":"+Contrasenia+"@tcp(127.0.0.1)/"+Nombre)
 
@@ -38,11 +38,11 @@ func main() {
 
 }
 
-type Users struct {
-	username  string
-	firstname string
-	lastname  string
-	birthdate string
+type User struct {
+	Username  string
+	Firstname string
+	Lastname  string
+	Birthdate string
 }
 
 func Init(w http.ResponseWriter, r *http.Request) {
@@ -62,17 +62,23 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 
 		username := r.FormValue("username")
+		password := r.FormValue("password")
+		confirmpwd := r.FormValue("confirmpwd")
 		firstname := r.FormValue("firstname")
 		lastname := r.FormValue("lastname")
-		birthdate := r.FormValue("birthday")
+		birthdate := r.FormValue("birthdate")
 
 		conexionEstablecida := conexionBD()
-		insertarRegistros, err := conexionEstablecida.Prepare("INSERT INTO users(username,lastname,firstname,birthdate) VALUES('?','?','?','?')")
+
+		insertarRegistros, err := conexionEstablecida.Prepare("INSERT INTO users(username,password,confirmpwd,firstname,lastname,birthdate) VALUES(?,?,?,?,?,?)")
 
 		if err != nil {
 			panic(err.Error())
 		}
-		insertarRegistros.Exec(username, lastname, firstname, birthdate)
+
+		insertarRegistros.Exec(username, password, confirmpwd, firstname, lastname, birthdate)
+
+		temp.ExecuteTemplate(w, "init", nil)
 
 		http.Redirect(w, r, "/", 301)
 
@@ -83,32 +89,48 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 func Information(w http.ResponseWriter, r *http.Request) {
 
 	conexionEstablecida := conexionBD()
+
 	registros, err := conexionEstablecida.Query("SELECT * FROM users")
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	user := Users{}
-	arregloUsers := []Users{}
+	user := User{}
+	arrayUser := []User{}
 
 	for registros.Next() {
-
 		var username string
-		var lastname string
+		var password string
+		var confirmpwd string
 		var firstname string
+		var lastname string
 		var birthdate string
-		err = registros.Scan(&username, &lastname, &firstname, &birthdate)
+		err = registros.Scan(&username, &password, &confirmpwd, &firstname, &lastname, &birthdate)
+
 		if err != nil {
 			panic(err.Error())
 		}
-		user.username = username
-		user.lastname = lastname
-		user.firstname = firstname
-		user.birthdate = birthdate
 
-		arregloUsers = append(arregloUsers, user)
+		user.Username = username
+		user.Firstname = firstname
+		user.Lastname = lastname
+		user.Birthdate = birthdate
+
+		arrayUser = append(arrayUser, user)
+
+		if r.Method == "GET" {
+
+			UserName := r.FormValue("usname")
+			PassWord := r.FormValue("passw")
+
+			if UserName != user.Username {
+
+			}
+
+		}
 	}
-	temp.ExecuteTemplate(w, "information", arregloUsers)
+
+	temp.ExecuteTemplate(w, "information", arrayUser)
 
 }
